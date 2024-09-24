bce0444112ec693725e9d60e2dfc5caef55e9c78
Simeon Widdis <sawiddis@amazon.com>
2024-09-24 16:13:44 -0700

    Fix usage of credentials fixture
    Signed-off-by: Simeon Widdis <sawiddis@amazon.com>

--- tests/test_opensearch_serverless.py
+++ tests/test_opensearch_serverless.py
@@ -12,14 +12,14 @@ sql_cli_vcr = vcr.VCR(
 
 class TestServerless:
     @pytest.fixture(scope="function")
-    def aws_credentials(self):
+    def aws_serverless_credentials(self):
         os.environ["TEST_ENDPOINT_URL"] = "https://example_endpoint.beta-us-east-1.aoss.amazonaws.com:443"
         os.environ["AWS_ACCESS_KEY_ID"] = "testing"
         os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
         os.environ["AWS_SESSION_TOKEN"] = "testing"
 
 
-    def test_show_tables(aws_credentials):
+    def test_show_tables(self, aws_serverless_credentials):
         with sql_cli_vcr.use_cassette("serverless_show_tables.yaml"):
             aes_test_executor = OpenSearchConnection(endpoint=os.environ["TEST_ENDPOINT_URL"], use_aws_authentication=True)
             aes_test_executor.set_connection()
